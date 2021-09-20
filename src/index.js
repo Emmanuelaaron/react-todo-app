37adb360b4340daa58ea039b6ad8f84457d236ca
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-09-20 11:17:53 +0300

    adds todoslist component

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom"
 
 import TodoContainer from "./components/TodoContainer";
 
-ReactDOM.render(<TodoContainer/>, document.getElementById("root"))
+ReactDOM.render(
+    <React.StrictMode>
+        <TodoContainer/>
+    </React.StrictMode>, 
+    document.getElementById("root")
+)
