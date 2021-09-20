4cff8765f0bf761f89052270656305e2e63bfa58
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-09-20 12:26:14 +0300

    adds provision for deleting a todo

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -21,11 +21,39 @@ class TodoContainer extends React.Component {
               }
         ]
     }
+    handleChange = id => {
+        this.setState(prevState => ({
+            todos: prevState.todos.map(todo => {
+              if (todo.id === id) {
+                  return{
+                      ...todo,
+                      completed: !todo.completed
+                  }
+                
+              }
+              return todo
+            }),
+        }))
+    }
+
+    delTodo = id => {
+        this.setState({
+            todos: [
+                ...this.state.todos.filter(todo => {
+                    return todo.id !== id
+                })
+            ]
+        })
+    }
     render(){
         return (
             <React.Fragment>
                 <Header/>
-                <TodosList todos={this.state.todos}/>
+                <TodosList 
+                todos={this.state.todos} 
+                handleChangeProps={this.handleChange}
+                deleteTodoProps={this.delTodo}
+                />
             </React.Fragment>
         )
     }
